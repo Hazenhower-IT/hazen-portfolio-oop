6d762c502b2463efe6ced682c24de947716e9354
Hazenhower-IT <118206118+Hazenhower-IT@users.noreply.github.com>
2023-12-31 17:54:57 +0100

    third test trying to fix vr selection handle

--- app.js
+++ app.js
@@ -1092,7 +1092,7 @@ class App{
 
       //TRY WITHOUS PLANE
       //intersects = this.raycaster.intersectObjects([this.plane])
-      intersects = this.raycaster.intersectObjects([this.scene], true)
+      intersects = this.raycaster.intersectObjects([this.scene.children], true)
 
       this.intersectUI = this.raycastUI()
       
